0c730ffa7d79bc52912cc7bda17c560dcb41a000
Skwangles <tree_house@hotmail.co.nz>
2022-03-16 12:20:46 +1300

    Re-added the TrieNode class to the Encode file - the Java compiler was having issues locating the TrieNode class in TrieNode.java.
    Added default method implementations to decode, pack and unpack - Main & ReadIn
    
    --WORKING VERISON--
    However, will likely modify to correctly implement the trie - i.e. Doing most of the navigateTrie process in the static context.

--- src/com/skwangles/LZWdecode.java
+++ src/com/skwangles/LZWdecode.java
@@ -1,4 +1,24 @@
 package com.skwangles;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class LZWdecode {
+
+    public static void main(String[] args){
+
+    }
+
+    private static void readInInput(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text;
+        try {
+            while ((text = br.readLine()) != null) {
+                //Dosomething
+            }
+        }
+        catch (Exception e){
+            System.out.println("Exception Thrown: " + e);
+        }
+    }
 }
--- src/com/skwangles/LZWpack.java
+++ src/com/skwangles/LZWpack.java
@@ -1,4 +1,23 @@
 package com.skwangles;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class LZWpack {
+    public static void main(String[] args){
+
+    }
+
+    private static void readInInput(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text;
+        try {
+            while ((text = br.readLine()) != null) {
+                //Dosomething
+            }
+        }
+        catch (Exception e){
+            System.out.println("Exception Thrown: " + e);
+        }
+    }
 }
