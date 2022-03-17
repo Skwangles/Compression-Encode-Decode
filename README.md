abebef67f407573fa9a5abc8d7d7e6c958f2ae5d
liamlabuschagne <liaml@disroot.org>
2022-03-17 21:57:38 +1300

    Working decoder and added corresponding instructions to README.md

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # COMPX301-LZW-Encode-Decode
 LZW Compression algorithm - Encodes, Decodes (Extra feature: Bitpacking included)
 
-cat \<file\> | java LZWencode -> will encode HEX to a series of LZW Phrase Numbers (0-9A-F)
+cat \<file\> | java LZWencode -> will encode HEX (0-9A-F) to a series of LZW Phrase Numbers
 
 cat \<file\> | java LZWdecode -> will decode LZW Phrase Numbers to HEX
+
+# Test encoding/decoding in one line:
+echo "AAABC00FFA2" | java LZWencode.java | java LZWdecode.java
+
+# Doing it a ridiculous number of times:
+echo "AAABC00FFA2" | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java | java LZWencode.java | java LZWdecode.java
--- src/com/skwangles/LZWdecode.java
+++ src/com/skwangles/LZWdecode.java
@@ -1,24 +1,87 @@
 package com.skwangles;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class LZWdecode {
 
-    public static void main(String[] args){
+    static class Dictionary {
+        // For testing with simpler alphabet
+        // List<Integer> phraseNumbers = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0));
+        // List<Character> followedBy = new ArrayList<>(Arrays.asList('\0', 'a', 'b',
+        // 'c', 'd'));
 
-    }
+        List<Integer> phraseNumbers = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+        List<Character> followedBy = new ArrayList<>(
+                Arrays.asList('\0', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'));
 
-    private static void readInInput(){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String text;
-        try {
-            while ((text = br.readLine()) != null) {
-                //Dosomething
+        public char getFirstChar(int phraseNumber) {
+            if (phraseNumbers.get(phraseNumber) == 0) {
+                return followedBy.get(phraseNumber);
+            } else {
+                return getFirstChar(phraseNumbers.get(phraseNumber));
             }
         }
-        catch (Exception e){
-            System.out.println("Exception Thrown: " + e);
+
+        public void printPhrase(int phraseNumber) {
+            if (phraseNumber > phraseNumbers.size()) {
+                System.err.print("Invalid input");
+                return;
+            } else if (phraseNumbers.get(phraseNumber) == 0) {
+                System.out.print(followedBy.get(phraseNumber));
+            } else {
+                // First print the nested phrase
+                printPhrase(phraseNumbers.get(phraseNumber));
+
+                // Then print this phrase's followedBy character
+                System.out.print(followedBy.get(phraseNumber));
+            }
         }
+
+        public void add(int phraseNumber) {
+            phraseNumbers.add(phraseNumber);
+            followedBy.add('\0');
+        }
+
+        public void set(int phraseNumber, char c) {
+            followedBy.set(phraseNumber, c);
+        }
+
+        public String toString() {
+            return phraseNumbers + "\n" + followedBy;
+        }
+
+        public int getLastPhraseNumber() {
+            return phraseNumbers.size() - 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Dictionary dict = new Dictionary();
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean first = true;
+
+        while (scanner.hasNext()) {
+            int phraseNumber = Integer.parseInt(scanner.nextLine()) + 1;
+
+            if (first) {
+                first = false;
+            } else {
+                char firstCharacter = dict.getFirstChar(phraseNumber);
+                // Set the followedBy character to the first character of this phrase
+                dict.set(dict.getLastPhraseNumber(), firstCharacter);
+            }
+
+            dict.printPhrase(phraseNumber);
+
+            // Create new empty phrase
+            dict.add(phraseNumber);
+        }
+
+        scanner.close();
     }
 }
