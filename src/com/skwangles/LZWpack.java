e35b12c33c2824dc4875e7ac16d9ea249138f3d6
Skwangles <tree_house@hotmail.co.nz>
2022-03-18 20:23:21 +1300

    Still bug fixing, i.e. I fixed the math of the first case in the bytePrint function. Still need to do some more bug fixing

--- src/com/skwangles/LZWpack.java
+++ src/com/skwangles/LZWpack.java
@@ -1,54 +1,58 @@
 package com.skwangles;
 //Alexander Stokes - 1578409, Liam Labuschagne - 1575313
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LZWpack {
     private static final int sizeOfByte = 8;
-    private static int currentMaxBitsNum = 5;
+    private static int currentMaxBitsNum = 5;//initally must start with 5
     private static int countOfPhrases = 0;
     private static byte outByte = 0;
     private static int outByteused = 0;
+    private static ArrayList<Byte> byteList;
 
     public static void main(String[] args){
+        byteList = new ArrayList<>();
         readInInput();//Start the stream
     }
 
     private static void readInInput(){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
         String text;
         try {
-            while ((text = br.readLine()) != null) {
-                bytePrint(Integer.parseInt(text)+1);//Stream value through - Adding 1 to use non-zero based indexes
+            while (scanner.hasNext()) {
+                text = scanner.next();
+                bytePrint(Integer.parseInt(text));//Stream value through - Adding 1 to use non-zero based indexes
             }
         }
         catch (Exception e){
             System.out.println("Exception Thrown: " + e);
         }
         endOfFile();//Makes sure all remaining bits are printed
-
+        scanner.close();
     }
 
     private static void bytePrint(int input){
         updateMaxBitsNum(); //Update currentMaxBits
         if((sizeOfByte - outByteused) < currentMaxBitsNum){//See if remainder of space can fit the input
-            byte temp = (byte) (input << (sizeOfByte-(currentMaxBitsNum % (sizeOfByte-outByteused))));//Creates the overflow value
-            byte sizeAdjustInput = (byte) (input >> (currentMaxBitsNum % (sizeOfByte-outByteused)));//Shifts the bits to fit in the remaining space
+            byte temp = (byte) (input << (2*sizeOfByte-currentMaxBitsNum-outByteused));//Creates the overflow value- also makes sure it is not signed
+            byte sizeAdjustInput = (byte) (input >> (currentMaxBitsNum - (sizeOfByte-outByteused)));//Shifts the bits to fit in the remaining space
             outByte = (byte) (outByte | sizeAdjustInput);
-            System.out.write(outByte);
+            printOut(outByte);
             outByte = temp;//
             outByteused = (currentMaxBitsNum % (sizeOfByte-outByteused));
         }
         else if((sizeOfByte-outByteused) == currentMaxBitsNum) {
     //int will fit perfectly - no wiggle room
             //Do stuff
-            System.out.write(outByte);
+            printOut(outByte);
             outByte = 0;//Cleans the byte to avoid carried bits
             outByteused = 0;//Finally, resets the amount of the byte used to 0
         }
         else{//The int will fit easily - but not finish the byte
-            outByte = (byte) (outByte |(input << ((sizeOfByte-outByteused)-currentMaxBitsNum)));//Aligns bits and ORs them
+            byte sizeAdjusted = (byte) (input << ((sizeOfByte-outByteused)-currentMaxBitsNum));
+            outByte = (byte) (outByte | sizeAdjusted);//Aligns bits and ORs them
             outByteused += currentMaxBitsNum;//Increase the amount used
         }
     }
@@ -56,15 +60,24 @@ public class LZWpack {
     private static void endOfFile(){//Ensure any partially filled bytes are printed
         if(outByte > 0){//Checks if a byte is partially formed - otherwise won't do anything
             outByte = (byte) (outByte & (0xFF << sizeOfByte-outByteused));//Creates a bitmask and cleans the byte to ensure no unexpected bits are in the remainder
-            System.out.write(outByte);
+            printOut(outByte);
         }//Only prints any remaining important bits, leaves the rest as 0
+        for (byte b: byteList) {
+            System.out.println(Integer.toBinaryString(b));
+        }
+        System.out.println(byteList.toString());
     }
 
     private static void updateMaxBitsNum(){//Updates and Returns the current Mac number of bits number should take up.
-        double value = Math.log(countOfPhrases + 16)/Math.log(2) + 1;
-        if(value % 1 == 0){//Checks if number is whole
-            currentMaxBitsNum = (int) value;//Cast whole number to Int
-        }
+//        double value = Math.log(countOfPhrases + 17)/Math.log(2) + 1;
+//        if(value % 1 == 0){//Checks if number is whole
+//            currentMaxBitsNum = (int) value;//Cast whole number to Int
+//        }
         countOfPhrases++;//Increments the amount of phrase numbers passed through
+    }
+
+    private static void printOut(byte outbyte){
+        byteList.add(outbyte);
+        //System.out.println(outbyte);
     }
 }
