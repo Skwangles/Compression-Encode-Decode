9251dc0a26d19ed26608b49852f7798689daffe2
Skwangles <tree_house@hotmail.co.nz>
2022-03-19 15:35:34 +1300

    Reimplemented Non-zero based indexes, reimplemented streaming the values (out as soon as it is ready), and adjusted the program to work with 32bit Integers.
    Writes out via System.out.write

--- src/com/skwangles/LZWpack.java
+++ src/com/skwangles/LZWpack.java
@@ -1,89 +1,78 @@
 package com.skwangles;
 //Alexander Stokes - 1578409, Liam Labuschagne - 1575313
-
-import java.util.ArrayList;
+//Alexander developed this part
 import java.util.Scanner;
 
 public class LZWpack {
-    private static final int sizeOfByte = 32;
+    private static final int outputBitCount = 32;//32bit Integers
     private static final int initalDictSize = 17;//Is 17, because we are operating with non-zerobased values (0 is escape)
-    private static int currentMaxBitsNum;
+    private static int bitsRequired;
     private static int countOfPhrases;
     private static int outByte;
-    private static int outByteused;
-    private static ArrayList<Integer> byteList;//Contains all bytes (converted to ints) output by the program
+    private static int bitsInUse;
 
     public static void main(String[] args){
-        byteList = new ArrayList<>();
-//        countOfBytesToInt = 0;
-//        bytesToInt = new byte[4];//4 bytes will go to 1 int
-        outByteused = 0;//The full 8bytes are unused
+        //Setup
+        bitsInUse = 0;//The full 8bytes are unused
         outByte = 0;//Has no value
         countOfPhrases = 0;
-        currentMaxBitsNum = 5;//initally must start with 5
-        readInInput();//Start the stream
-    }
+        bitsRequired = 5;//initally must start with 5
 
-    private static void readInInput(){
+        //
+        //Read in - starting the program
+        //
         Scanner scanner = new Scanner(System.in);
         String text;
         try {
             while (scanner.hasNext()) {
                 text = scanner.next();
-                bytePrint(Integer.parseInt(text));//Stream value through - Adding 1 to use non-zero based indexes (Reserves 0 as escape char)
+                packToBinary(Integer.parseInt(text)+1);//Stream value through - Adding 1 to use non-zero based indexes (Reserves 0 as escape char)
             }
         }
         catch (Exception e){
             System.out.println("Exception Thrown: " + e);
         }
-        endOfFile();//Makes sure all remaining bits are printed
         scanner.close();
 
         //
-        //Prints the values stored in the ArrayList
+        //Will add the last byte only if is not 0 - the ending of a byte that may be only 0s will not be printed, but both negative and positive integers will be printed
         //
-        for (int b: byteList) {
-            System.out.println(Integer.toBinaryString(b));
+        if(bitsInUse != 0){
+            printOut(outByte);
         }
-        System.out.println(byteList.toString());
     }
 
-    private static void bytePrint(int input){
-        updateMaxBitsNum(); //Update currentMaxBits
-        if((sizeOfByte - outByteused) < currentMaxBitsNum){//See if remainder of space can fit the input
-            int temp =  (input << (2*sizeOfByte-currentMaxBitsNum-outByteused));//Creates the overflow value
-            int sizeAdjustInput = (input >> (currentMaxBitsNum - (sizeOfByte-outByteused)));//Shifts the bits to fit in the remaining space
-            outByte =  (outByte | sizeAdjustInput);
-            addToOut(outByte);
+    private static void packToBinary(int input){
+        updateBitsRequired(); //Update currentMaxBits
+        System.out.println(input + " " +countOfPhrases + " " + bitsRequired);
+        if((outputBitCount - bitsInUse) < bitsRequired){//See if remainder the of space can fit the input
+            int temp =  (input << (2* outputBitCount - bitsRequired - bitsInUse));//Creates the overflow value
+            int shiftedInput = (input >> (bitsRequired - (outputBitCount - bitsInUse)));//Shifts the bits to fit in the remaining space
+            outByte =  (outByte | shiftedInput);
+            printOut(outByte);
             outByte = temp;//
-            outByteused = currentMaxBitsNum + outByteused - sizeOfByte;
+            bitsInUse = bitsRequired + bitsInUse - outputBitCount;
         }
-        else if((sizeOfByte-outByteused) == currentMaxBitsNum) {
+        else if((outputBitCount - bitsInUse) == bitsRequired) {
             //int will fit perfectly - no wiggle room
-            addToOut(outByte);
+            printOut(outByte);
             outByte = 0;//Cleans the byte to avoid carried bits
-            outByteused = 0;//Finally, resets the amount of the byte used to 0
+            bitsInUse = 0;//Finally, resets the amount of the byte used to 0
         }
         else{//The int will fit easily - but not finish the byte
-            int sizeAdjusted = (input << ((sizeOfByte-outByteused)-currentMaxBitsNum));
-            outByte = (outByte | sizeAdjusted);//Aligns bits and ORs them
-            outByteused += currentMaxBitsNum;//Increase the amount used
+            int shiftedInput = (input << ((outputBitCount - bitsInUse)- bitsRequired));//Aligns bits with the next unused space
+            outByte = outByte | shiftedInput;//ORs the bits together
+            bitsInUse += bitsRequired;//Increase the amount used
         }
     }
 
-    private static void endOfFile(){//Ensure any partially filled bytes are printed
-        if(outByteused > 0 && outByteused < sizeOfByte ){//Checks if a byte is partially formed - otherwise won't do anything (not checking the value of outbyte as it may have straight 0s that are important)
-            outByte = outByte & (0xFF << sizeOfByte-outByteused);//Creates a bitmask and cleans the byte to ensure no unexpected bits are in the remainder
-            addToOut(outByte);
-        }
-    }
-
-    private static void updateMaxBitsNum(){//Updates and Returns the current Mac number of bits number should take up.
-        currentMaxBitsNum = (int) Math.ceil(Math.log(countOfPhrases + initalDictSize)/Math.log(2));//Gets the no of bits required
+    private static void updateBitsRequired(){//Updates and Returns the current Mac number of bits number should take up.
+        bitsRequired = (int) Math.ceil(Math.log(countOfPhrases + initalDictSize)/Math.log(2));//Gets the no of bits required
         countOfPhrases++;//Increments the amount of phrase numbers passed through
+
     }
 
-    private static void addToOut(int outbyte){
-        byteList.add(outbyte);
+    private static void printOut(int outByte){
+        System.out.write(outByte);
     }
 }
