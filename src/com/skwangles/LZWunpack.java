87a095c25eb9f3063338e3cdcf483616a388a04c
Skwangles <tree_house@hotmail.co.nz>
2022-03-19 15:48:41 +1300

    Added common methods to the unpack file - e.g. updateBitsRequired - calculates the number of bits per phrasenumber, along with the associated static variables.

--- src/com/skwangles/LZWunpack.java
+++ src/com/skwangles/LZWunpack.java
@@ -5,11 +5,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class LZWunpack {
+    private static final int initalDictSize = 17;//Is 17, because we are operating with non-zerobased values (0 is escape)
+    private static int bitsRequired;//Holds the expected size of the next phrase number
+    private static int countOfPhrases;//Holds the count of values that have been parsed
+
     public static void main(String[] args){
+        //Initalising values
+        bitsRequired = 5;
+        countOfPhrases = 0;
 
-    }
-
-    private static void readInInput(){
+        //
+        //Read In
+        //
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String text;
         try {
@@ -20,5 +27,11 @@ public class LZWunpack {
         catch (Exception e){
             System.out.println("Exception Thrown: " + e);
         }
+    }
+
+
+    private static void updateBitsRequired(){//Updates and Returns the current number of bits a phrase number should take up.
+        bitsRequired = (int) Math.ceil(Math.log(countOfPhrases + initalDictSize)/Math.log(2));//Gets the no of bits required
+        countOfPhrases++;//Increments the amount of phrase numbers passed through
     }
 }
