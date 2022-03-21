b17866ba6347f5b585bd176ad49057c4d61d5c24
Skwangles <tree_house@hotmail.co.nz>
2022-03-21 13:16:38 +1300

    Commenting, and formatting

--- src/com/skwangles/LZWencode.java
+++ src/com/skwangles/LZWencode.java
@@ -5,16 +5,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class LZWencode {
-    //public static char[] applicableChars = { 'A', 'B', 'C', 'D'};//To test with tony's numbers
-    public static char[] applicableChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};//NEED TO CHECK IF A SPACE BETWEEN HEX is applicable
+    public static char[] applicableChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
     public static StringBuilder input;
     public static int phrases;
-    public static void main(String[] args){//Static main function allow LZWencode to be a standalone file
+    public static void main(String[] args){
         //
         //Setup
         //
         input = new StringBuilder();
-        phrases = 1;//Defines the starting phrase number - 0 is nothing, 1 is 0, 2 is 1, etc - need the 0 for bit packing, otherwise uneven output may yield a bunch of phrase 0s
+        phrases = 1;//Defines the starting phrase number - Tony said we can pick 0 or 1 as the start, we opted for 1.
         TrieNode root = new TrieNode('\0', -1);//Root has no applicable phrase number
         for (char c: applicableChars) {
             root.AddChild(c, getPhraseNumber());//Creates Node, and assigns phraseNumber
@@ -43,7 +42,7 @@ public class LZWencode {
         }
     }
 
-    public static int getPhraseNumber(){
+    public static int getPhraseNumber(){//Updates the phrase number
         phrases++;
         return phrases -1;
     }
@@ -69,7 +68,7 @@ class TrieNode {
         this.children[index] = new TrieNode(c, phraseNum);
     }
 
-    public int navigateTrie(){//Uses the static value of input - and slowly chips off the chars as it is parsed.
+    public int navigateTrie(){//Uses the static value of the complete input - and slowly chips off the chars as it is parsed.
         if(LZWencode.input.length() > 0){//String must have at least 1 char - otherwise end of input is found
             for (TrieNode child : children) {
                 if(child == null) break; //If null item, the rest of the array will be Null
